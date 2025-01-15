5efed468a4219599f11e0d0090f5b8ed89a8e759
karlpnord <karnor50544@stud.noroff.no>
2025-01-15 23:21:13 +0100

    CV update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='h-[72px] px-4 flex items-center justify-between sticky top-0 z-20 bg-bgLight bg-opacity-50 backdrop-blur-md'>
       <MyLinks />
-      <OutlineButton handler={() => window.open('/Karl_Petter_Nordgaard_CV.pdf')}>My Resume</OutlineButton>
+      <OutlineButton handler={() => window.open('/CV_Karl_Nordgaard_English.pdf')}>My Resume</OutlineButton>
     </header>
   );
 };
